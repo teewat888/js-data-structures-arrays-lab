e081e4ce3bc0782d0c37249092531e2234b3984a
teerawat prarom <teewat@yahoo.com>
2021-07-25 23:31:22 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const drivers = ["Milo","Otis","Garfield"];
+
+const destructivelyAppendDriver = (name)=>{
+    drivers.push(name);
+}
+
+const destructivelyPrependDriver = (name)=>{
+    drivers.unshift(name);
+}
+
+const destructivelyRemoveLastDriver = ()=>{
+    drivers.pop();
+}
+
+const destructivelyRemoveFirstDriver = ()=>{
+    drivers.shift();
+}
+
+const appendDriver = (name)=>{
+    const newDrivers = [...drivers];
+    newDrivers.push(name);
+    return newDrivers
+}
+
+const prependDriver = (name)=>{
+    const newDrivers = [...drivers];
+    newDrivers.unshift(name);
+    return newDrivers
+}
+
+const removeLastDriver = ()=>{
+    const newDrivers = [...drivers];
+    newDrivers.pop();
+    return newDrivers
+}
+
+const removeFirstDriver = ()=>{
+    const newDrivers = [...drivers];
+    newDrivers.shift();
+    return newDrivers
+}
